0d29bc3ee725f6eed90da6ed9bf134f90fba8a95
Cortes J <jacobcortes46@gmail.com>
2025-04-02 15:02:13 +0800

    vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './Header.jsx'
-import Body1 from './body1.jsx'
-import Body2 from './body2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Header />
-      <Body1 />
-      <Body2 />
   </React.StrictMode>
 )
