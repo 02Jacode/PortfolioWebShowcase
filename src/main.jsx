5f3f700809d556860110a1e4b0e31446a3d05182
Cortes J <jacobcortes46@gmail.com>
2025-04-02 14:52:09 +0800

    fjhjfh

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './App.jsx'
 import Header from './Header.jsx'
 import Body1 from './body1.jsx'
 import Body2 from './body2.jsx'
